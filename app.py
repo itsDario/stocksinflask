060f2204b371e5a24330ebed482c2da4ea7aebfe
github username <itsDario1@gmail.com>
2019-09-28 14:54:17 -0400

    setup basic routes and templates

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from data import Stocks
+
+Stocks = Stocks()
 
 app = Flask(__name__)
 
@@ -6,6 +9,21 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/stocks')
+def stocks():
+    return render_template('stocks.html', stocks=Stocks)
+
+
+@app.route('/stock/<string:id>/')
+def stock(id):
+    return render_template('stock.html', id=id)
 
 
 if __name__ == '__main__':
---
+++ data.py
@@ -0,0 +1,21 @@
+def Stocks():
+    stocks = [{
+        'id': 1,
+        'title': 'article one',
+        'body': 'lorem',
+        'author': 'mario',
+        'create_date': '04-45-2222'
+    }, {
+        'id': 2,
+        'title': 'article two',
+        'body': 'lorem',
+        'author': 'mario',
+        'create_date': '04-45-2222'
+    }, {
+        'id': 3,
+        'title': 'article three',
+        'body': 'lorem',
+        'author': 'mario',
+        'create_date': '04-45-2222'
+    }]
+    return stocks
